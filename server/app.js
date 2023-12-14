77b7e43bc8705ca135a33e90fa07986bfab19e3b
James Lindsay <jameslindsaycode@gmail.com>
2023-12-14 01:01:30 +0000

    added getUser, getCart and myLocation methods

--- server/app.js
+++ server/app.js
@@ -15,35 +15,57 @@ var USER_PROTO_PATH = __dirname + "../../protos/user.proto";
 var userPackageDefinition = protLoader.loadSync(USER_PROTO_PATH);
 var user_proto = grpc.loadPackageDefinition(userPackageDefinition).user;
 
+//UserService members and functions
 var users = []; // Set-up user data array globally
 var activeCartId = 0; //counter starts at zero and will be incremented for every new login call
+var userId;
+//var aisle;
+//var shelf;
 
+function validUserId(userId) {
+  userId != 0 && userId.toString().length == 4; //validation for userId
+}
+function findUser(userId) {
+  users.find((x) => x.id === userId);
+}
 // function to add a user to the users object array
 function setUser(userId, name, activeCartId) {
   users.push({ id: userId, name: name, activeCartId: activeCartId });
+}
+
+//function to initialise the user's location in the store
+function setLocation(userId) {
+  if (findUser(userId)) {
+    users.push({ aisle: 1, shelf: 1 });
+  }
+}
+//function to update users location in the store from client call
+function updateLocation(userId, aisle, shelf) {
+  var myUser = users.find((x) => x.id === userId);
+  myUser.aisle === aisle;
+  myUser.location === shelf;
 }
 
 function login(call, callback) {
   try {
     var userId = parseInt(call.request.userId);
     var name = call.request.name;
-    var validUserId = userId != 0 && userId.toString().length == 4; //validation for userId
+    var validUserId = userId != 0 && userId.toString().length == 4;
     var result;
     var activeUser;
-    var findUser;
-    //console.log(userId + ": " + name);
+    //var findUser = findUser(userId);
+    console.log(userId + ": " + name + "just logged in!");
     if (validUserId && name != null) {
-      findUser = users.find((x) => x.id === userId);
+      var findUser = users.find((x) => x.id === userId);
       if (!findUser) {
         activeCartId++;
-        console.log(activeCartId);
+        console.log("Cart Id: " + activeCartId);
         setUser(userId, name, activeCartId);
         activeUser = users.find((x) => x.id === userId);
-        var result =
-          "Welcome, " + activeUser.name + ": Please enter the store :)";
+        result = "Welcome, " + activeUser.name + ": Please enter the store :)";
       } else {
         activeUser = users.find((x) => x.id === userId);
-        var result =
+        result =
           activeUser.name +
           ": You are already logged in, please enter the store :)";
       }
@@ -68,22 +90,88 @@ function login(call, callback) {
 
 function getUser(call, callback) {
   try {
-    var userId = parseString(call.request.user);
-    if (userId != null) {
-      var result = "We have found your profile, " + userId;
+    userId = parseInt(call.request.userId);
+    var findUser = users.find((x) => x.id === userId);
+    if (userId != null && findUser) {
+      var result =
+        "I have found your profile, " +
+        findUser.name +
+        ". You are using cart id: " +
+        findUser.activeCartId;
       callback(null, {
         message: undefined,
         result: result,
       });
     } else {
       callback(null, {
-        message:
+        message: undefined,
+        result:
           "Please enter a valid userId or visit www.smart-retail.com/register",
       });
     }
   } catch (e) {
     callback(null, {
-      message: "An error occured during computation",
+      message: undefined,
+      result: "An error occured during computation",
+    });
+  }
+}
+
+function myLocation(call, callback) {
+  try {
+    userId = parseInt(call.request.userId);
+    var aisle = parseInt(call.request.aisle);
+    var shelf = parseInt(call.request.shelf);
+    console.log(aisle);
+    console.log(shelf);
+    var findUser = users.find((x) => x.id === userId);
+    if (userId != null && findUser) {
+      var result = "Your location is aisle #" + aisle + ", shelf #" + shelf;
+      callback(null, {
+        message: undefined,
+        result: result,
+      });
+    } else {
+      callback(null, {
+        message: undefined,
+        result:
+          "Please enter a valid userId or visit www.smart-retail.com/register",
+      });
+    }
+  } catch (e) {
+    callback(null, {
+      message: undefined,
+      result: "An error occured during computation",
+    });
+  }
+}
+
+// ****Cart functions*****
+function getCart(call, callback) {
+  try {
+    var userId = parseInt(call.request.userId);
+    var findUser = users.find((x) => x.id === userId);
+    if (userId != null && findUser) {
+      var result =
+        "I have found your cart, " +
+        findUser.name +
+        ". You are using cart id: " +
+        findUser.activeCartId;
+      callback(null, {
+        message: undefined,
+        result: result,
+      });
+    } else {
+      callback(null, {
+        message: undefined,
+        result:
+          "Please enter a valid userId or visit www.smart-retail.com/register",
+      });
+    }
+  } catch (e) {
+    callback(null, {
+      message: undefined,
+      result: "An error occured during computation",
     });
   }
 }
@@ -93,16 +181,16 @@ var server = new grpc.Server();
 server.addService(user_proto.UserService.service, {
   login: login,
   getUser: getUser,
+  myLocation: myLocation,
 });
 
-/*
 server.addService(cart_proto.CartService.service, {
-  createCart: createCart,
-  addItem: addItem,
-  removeItem: removeItem,
+  //createCart: createCart,
+  //addItem: addItem,
+  //removeItem: removeItem,
   getCart: getCart,
 });
-
+/*
 server.addService(checkout_proto.CheckoutService.service, {
   checkoutRequest: checkoutRequest,
 });
--- web-client/routes/index.js
+++ web-client/routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 var grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
 
-/*
-var CART_PROTO_PATH = __dirname + "../../protos/cart.proto";
+var CART_PROTO_PATH = __dirname + "../../../protos/cart.proto";
 var cartPackageDefinition = protoLoader.loadSync(CART_PROTO_PATH);
 var cart_proto = grpc.loadPackageDefinition(cartPackageDefinition).cart;
-
+/*
 var CHECKOUT_PROTO_PATH = __dirname + "../../protos/checkout.proto";
 var checkoutPackageDefinition = protoLoader.loadSync(CHECKOUT_PROTO_PATH);
 var checkout_proto = grpc.loadPackageDefinition(
@@ -19,6 +18,11 @@ var userPackageDefinition = protoLoader.loadSync(USER_PROTO_PATH);
 var user_proto = grpc.loadPackageDefinition(userPackageDefinition).user;
 
 var user_client = new user_proto.UserService(
+  "0.0.0.0:4000",
+  grpc.credentials.createInsecure()
+);
+
+var cart_client = new cart_proto.CartService(
   "0.0.0.0:4000",
   grpc.credentials.createInsecure()
 );
@@ -62,6 +66,137 @@ router.get("/", function (req, res, next) {
   } else {
     res.render("index", {
       title: "Smart Retail",
+      error: null,
+      result: result,
+    });
+  }
+});
+
+/* User Page */
+router.get("/user", function (req, res, next) {
+  var userId = req.query.userId;
+  // var name = req.query.name;
+  var result;
+
+  if (!isNaN(userId)) {
+    try {
+      user_client.getUser({ userId: userId }, function (error, response) {
+        try {
+          res.render("user", {
+            title: "Your User Details",
+            error: error,
+            result: response.result,
+          });
+        } catch (error) {
+          console.log(error);
+          res.render("user", {
+            title: "Your User Details",
+            error:
+              "Service is not available at the moment please try again later",
+            result: null,
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.render("user", {
+        title: "Your User Details",
+        error: "Service is not available at the moment please try again later",
+        result: null,
+      });
+    }
+  } else {
+    res.render("user", {
+      title: "Your User Details",
+      error: null,
+      result: result,
+    });
+  }
+});
+
+/* Cart Page */
+router.get("/cart", function (req, res, next) {
+  var userId = req.query.userId;
+  // var name = req.query.name;
+  var result;
+
+  if (!isNaN(userId)) {
+    try {
+      cart_client.getCart({ userId: userId }, function (error, response) {
+        try {
+          res.render("cart", {
+            title: "Your Shopping Cart",
+            error: error,
+            result: response.result,
+          });
+        } catch (error) {
+          console.log(error);
+          res.render("cart", {
+            title: "Your Shopping Cart",
+            error:
+              "Service is not available at the moment please try again later",
+            result: null,
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.render("cart", {
+        title: "Your Shopping Cart",
+        error: "Service is not available at the moment please try again later",
+        result: null,
+      });
+    }
+  } else {
+    res.render("cart", {
+      title: "Your Shopping Cart",
+      error: null,
+      result: result,
+    });
+  }
+});
+
+/* Location Page */
+router.get("/location", function (req, res, next) {
+  var userId = req.query.userId;
+  var aisle = Math.floor(Math.random() * 10) + 1;
+  var shelf = Math.floor(Math.random() * 10) + 1;
+  // var name = req.query.name;
+  var result;
+
+  if (!isNaN(userId)) {
+    try {
+      user_client.myLocation(
+        { userId: userId, aisle: aisle, shelf: shelf },
+        function (error, response) {
+          try {
+            res.render("location", {
+              title: "Your Location",
+              error: error,
+              result: response.result,
+            });
+          } catch (error) {
+            console.log(error);
+            res.render("location", {
+              title: "Your Location",
+              error:
+                "Service is not available at the moment please try again later",
+              result: null,
+            });
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.render("location", {
+        title: "Your Location",
+        error: "Service is not available at the moment please try again later",
+        result: null,
+      });
+    }
+  } else {
+    res.render("location", {
+      title: "Your Location",
       error: null,
       result: result,
     });
